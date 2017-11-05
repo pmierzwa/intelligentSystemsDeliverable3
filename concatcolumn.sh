e22311ae77c5329890a3b9a62e7814e93a3c02a1
pmierzwa <32395894+pmierzwa@users.noreply.github.com>
2017-11-05 14:03:02 +0100

    1st notebook done
    1st notebook done. 
    Sensors chosen method :  20 clusters. Take 1 sensor from each cluster (the one closest to the clusters center)

---
+++ concatcolumn.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cut -d ',' -f1 --complement energy_demand_data.csv | paste -d ','- exchange_data.csv > helper.csv
+cut -d ',' -f1 --complement generator_production_data.csv | paste -d ',' - helper.csv > all.csv
+cut -d ',' -f1 --complement exchange_data.csv | paste -d ',' - all.csv > helper.csv
+cut -d ',' -f1 --complement renewable_production_data.csv | paste -d ',' - helper.csv > all.csv
+rm -rf helper.csv
+
+ 
+
